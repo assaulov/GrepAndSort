d952868410623eac2510d44470404097967f4486
assaulov <assaulov.i@gmail.com>
2020-11-14 20:30:20 +0200

    working

--- src/main/java/GrepAndSort/Grep/Grep.java
+++ src/main/java/GrepAndSort/Grep/Grep.java
@@ -1,21 +1,18 @@
 package GrepAndSort.Grep;
 
-import GrepAndSort.Interfaces.Methods;
+
 import org.w3c.dom.ls.LSOutput;
 
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Grep implements Methods {
+public class Grep  {
     private String inputString;
     private String filter;
 
-    public Grep(String inputString, String filter) {
-        this.inputString = inputString;
-        this.filter = filter;
-    }
 
     public String getInputString() {
         return inputString;
@@ -33,27 +30,19 @@ public class Grep implements Methods {
         this.filter = filter;
     }
 
-    @Override
-    public void grep() {
-        if(getInputString().isEmpty() || getFilter().isEmpty()) {
+
+    public void grep(String inputString, String filter) {
+        if(inputString.isEmpty() || filter.isEmpty()) {
             System.out.println("Введите данные!");
         } else {
-            Stream<String> stringStream = Stream.of(getInputString().split("[\\p{Punct}\\s]+"));
-            String result = stringStream.filter(s -> s.contains(getFilter()))
+            Stream<String> stringStream = Stream.of(inputString.split("[\\p{Punct}\\s]+"));
+            String result = stringStream.filter(s -> s.contains(filter))
                     .map(String::toLowerCase)
                     .collect(Collectors.joining(", "));
             if(result.isEmpty()) {
                 System.out.println("Совпадений не найдено!");
             } else System.out.println(result);
-            /*stringStream.filter(s -> s.contains(getFilter()))
-                    .map(String::toLowerCase)
-                    .sorted()
-                    .forEach(s1 -> {
-                                if (s1.equalsIgnoreCase("")) {
-                                    System.out.println("Совпадений не найдено!");
-                                } else System.out.print(s1 + " ");
-                            } );
-                           //*/
+
         }
 
     }
--- src/main/java/GrepAndSort/Main.java
+++ src/main/java/GrepAndSort/Main.java
@@ -9,22 +9,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
+/*
         final String STOP = "stop";
         if (args.length == 0) {
-            Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar " + (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())).getAbsolutePath() + " cmd");
+            Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar " +
+                    (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())).getAbsolutePath() + " cmd");
 
         } else {
-
+*/
             while (true) {
-                Scanner scanner = new Scanner(System.in);
+                Grep grep = new Grep();
+                var scanner = new Scanner(System.in);
                 System.out.println("Введите слова: ");
-                var inputString = scanner.nextLine();
+                grep.setInputString(scanner.nextLine());
                 System.out.println("Введите фильтр: ");
-                var filter = scanner.nextLine().toLowerCase();
-                Grep grep = new Grep(inputString, filter);
+                grep.setFilter(scanner.nextLine().toLowerCase());
                 System.out.print("Результат применения фильтра: ");
-                grep.grep();
+                grep.grep(grep.getInputString(), grep.getFilter());
                 System.out.println("Если хотите продолжить нажмите любую клавишу. Введите \"stop\" для выхода.");
                 if(scanner.nextLine().equalsIgnoreCase("stop")){
                     scanner.close();
@@ -39,4 +40,4 @@ public class Main {
         }
 
     }
-}
+//}
